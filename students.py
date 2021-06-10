d466b0e25577f91830c93437287a355e9727ad96
alexgolovko <ya.golovko-alex2017@yandex.ru>
2021-06-10 23:55:02 +0300

    change in class Student, Lecturer, Reviewer

--- students.py
+++ students.py
@@ -7,8 +7,14 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
-    def add_courses(self, course_name):
-        self.finished_courses.append(course_name)
+    def rate_lecturer(self,lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 
 class Mentor:
@@ -16,6 +22,19 @@ class Mentor:
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname,):
+        super().__init__(name, surname)
+        self.courses_attached = []
+        self.grades = {}
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname,):
+        super().__init__(name, surname)
+        # self.courses_attached = []
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -25,7 +44,6 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
