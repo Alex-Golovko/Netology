cbaa8712ad9c2f8a4fc3a94ff714fa803a13702c
alexgolovko <ya.golovko-alex2017@yandex.ru>
2021-06-11 00:50:58 +0300

    Inspection code

--- students.py
+++ students.py
@@ -7,8 +7,9 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
-    def rate_lecturer(self,lecturer, course, grade=10):
-        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_attached:
+
+    def rate_lecturer(lecturer, course, grade=10):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
@@ -17,7 +18,11 @@ class Student:
             return 'Ошибка'
 
     def __str__(self):
-        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.grades}\nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}'
+        return f'Имя: {self.name}' \
+               f'Фамилия: {self.surname}' \
+               f'Средняя оценка за домашние задания: {self.grades}' \
+               f'Курсы в процессе изучения: {self.courses_in_progress}' \
+               f'Завершенные курсы: {self.finished_courses}'
 
 
 class Mentor:
@@ -28,19 +33,20 @@ class Mentor:
 
 
 class Lecturer(Mentor):
-    def __init__(self, name, surname,):
+    def __init__(self, name, surname, ):
         super().__init__(name, surname)
         self.courses_attached = []
         self.grades = {}
 
     def __str__(self):
-        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.grades}'
+        return f'Имя: {self.name}' \
+               f'Фамилия: {self.surname}' \
+               f'Средняя оценка за лекции: {self.grades}'
 
 
 class Reviewer(Mentor):
-    def __init__(self, name, surname,):
+    def __init__(self, name, surname, ):
         super().__init__(name, surname)
-
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -52,7 +58,9 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
     def __str__(self):
-        return f'Имя: {self.name}\n Фамилия: {self.surname}'
+        return f'Имя: {self.name}' \
+               f'Фамилия: {self.surname}'
+
 
 best_student = Student('Ruoy', 'Eman', 'man')
 best_student.courses_in_progress += ['Python']
