345feb131106f7b452eb0baa1f4494fe0b030f3f
alexgolovko <ya.golovko-alex2017@yandex.ru>
2021-06-12 00:20:44 +0300

    add rate_lecturer, rate_student, method __lt__

--- students.py
+++ students.py
@@ -8,14 +8,22 @@ class Student:
         self.grades = {}
 
 
-    def rate_lecturer(lecturer, course, grade=10):
-        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached:
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and self.courses_in_progress and self.finished_courses and course in lecturer.courses_attached and grade <= 10:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Не студент!')
+            return
+        return self.grades < other.grades
+
 
     def __str__(self):
         return f'Имя: {self.name}\n' \
@@ -38,22 +46,28 @@ class Lecturer(Mentor):
         self.courses_attached = []
         self.grades = {}
 
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Не лектор!')
+            return
+        return self.grades < other.grades
+
     def __str__(self):
         return f'Имя: {self.name}\n' \
-               f' Фамилия: {self.surname}\n' \
-               f' Средняя оценка за лекции: {str(self.grades)}'
+               f'Фамилия: {self.surname}\n' \
+               f'Средняя оценка за лекции: {str(self.grades)}'
 
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
 
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+    def rate_hw(self, student, course, grades):
+        if isinstance(student, Student) and course in lecturer.courses_attached and course in student.courses_in_progress and grades <= 10:
             if course in student.grades:
-                student.grades[course] += [grade]
+                student.grades[course] += [grades]
             else:
-                student.grades[course] = [grade]
+                student.grades[course] = [grades]
         else:
             return 'Ошибка'
 
@@ -63,18 +77,30 @@ class Reviewer(Mentor):
 
 
 student = Student('Ruoy', 'Eman', 'man')
+student_1 = Student('Alex', 'Golovko', 'man')
 student.courses_in_progress += ['Python', 'Git']
 student.finished_courses += ['Введение в программирование']
-student.grades = 10
-
+student_1.courses_in_progress += ['Python', 'Git']
+student_1.finished_courses += ['Введение в программирование']
 lecturer = Lecturer('Some', 'Buddy')
 lecturer.courses_attached += ['Python']
+
 reviewer = Reviewer('Some', 'Buddy')
 
-reviewer.rate_hw(student, 'Python', 10)
-reviewer.rate_hw(student, 'Python', 10)
-reviewer.rate_hw(student, 'Python', 10)
+student.rate_lecturer(lecturer, 'Python', 10)
+student_1.rate_lecturer(lecturer, 'Python', 10)
 
-print(student)
+
+
+
+reviewer.rate_hw(student, 'Python', 10)
+reviewer.rate_hw(student_1, 'Python', 10)
+
+
 print(lecturer)
+print()
+print(student)
+print()
+print(student_1)
+print()
 print(reviewer)
